d7f1d66cffc57389d51e425b146fbd6250f825b6
kuniaki <web.t.c.o.k@gmail.com>
2023-11-07 21:36:35 +0900

    publicフォル作成し静的ファイル追加、staticで静的ファイル読み込み

--- server/server.js
+++ server/server.js
@@ -11,17 +11,23 @@ dotenv.config();
 const host = process.env.HOST;
 const port = process.env.PORT;
 
-console.log(host, port)
+console.log(host, port);
+
+//ミドルウェア設定
+
+//staticで静的ファイルの読み込み
+app.use(express.static(__dirname + "/public"));
 
 //各API
 app.get("/" ,(req, res) => {
     res.send("hello");
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
 });
 
 
 //listnでサーバーを待機状態（起動）する
 app.listen(port, host, () => {
-    console.log("サーバー待機中");
-
-
+    console.log(`サーバー待機中 PORT:${port} HOST:${host}`);
 });
