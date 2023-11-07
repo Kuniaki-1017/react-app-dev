21405507f0196823567746f1cfdb5eb5d29fc1b2
kuniaki <web.t.c.o.k@gmail.com>
2023-11-07 21:49:45 +0900

    ulエンコードのミドルウェア実装、ログイン用のpostAPI実装

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ console.log(host, port);
 
 //staticで静的ファイルの読み込み
 app.use(express.static(__dirname + "/public"));
+//URLのエンコード化（マルチバイト文字の文字化け防止）
+app.use(express.urlencoded({extended: true}));
 
 //各API
 app.get("/" ,(req, res) => {
@@ -24,6 +26,18 @@ app.get("/" ,(req, res) => {
     console.log(req.body);
     console.log(req.url);
     console.log(req.query);
+});
+
+app.post("/auth", (req,res) => {
+    //フォームから送られた値を取得し変数に代入（name属性の値）
+    const login_name = req.body.login_name;
+    const password = req.body.password;
+
+    let message = "ログインできませんでした";
+    if(login_name === process.env.LOGIN_NAME && password === process.env.PASSWORD){
+        message = "ログインしました"
+    }
+    res.send(message);
 });
 
 
